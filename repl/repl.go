1f9503e273d38aeeab661799ca1708738579ff03
Bogdan Basarabeanu <basarabeanubogdan95@gmail.com>
2023-08-17 09:29:02 +0300

    eval integers, booleans, prefix & infix expressions & conditionals

--- repl/repl.go
+++ repl/repl.go
@@ -1,6 +1,7 @@
 package repl
 
 import (
+	"apeScript/evaluator"
 	"apeScript/lexer"
 	"apeScript/parser"
 	"bufio"
@@ -42,8 +43,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 
 	}
 }
